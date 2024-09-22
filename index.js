351845e72093279557b479a9f75bc393da0bd4f0
Yan Lucas <88909517+yanswo@users.noreply.github.com>
2024-09-22 11:53:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-idade = 18;
+let nome = "Robert";
+let xp = 1000;
 
-if (idade < 18) {
-  console.log("menor de idade");
-} else if (idade >= 18) {
-  console.log("maior de idade");
+function classificarHeroi(xpAtual) {
+  if (xpAtual <= 1000) {
+    return "Ferro";
+  } else if (xpAtual <= 2000) {
+    return "Bronze";
+  } else if (xpAtual <= 5000) {
+    return "Prata";
+  } else if (xpAtual <= 7000) {
+    return "Ouro";
+  } else if (xpAtual <= 8000) {
+    return "Platina";
+  } else if (xpAtual <= 9000) {
+    return "Ascendente";
+  } else if (xpAtual <= 10000) {
+    return "Imortal";
+  } else {
+    return "Radiante";
+  }
 }
+
+let nivel = classificarHeroi(xp);
+console.log(
+  `O Herói de nome: ${nome} está no nível de: ${nivel} com xp de: ${xp}!`
+);
